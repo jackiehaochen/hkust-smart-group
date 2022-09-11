6138ce218ccca5acf0e95767129904e23b2636eb
xiaoyu-z <xiaoyu.z.ch@gmail.com>
2022-09-11 20:06:28 +0800

    test publication page

---
+++ content/publication/html_publication_generation.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('test.csv', newline='') as csvfile:
+  reader = csv.DictReader(csvfile)
+  total_paper = 0
+  for row in reader:
+    total_paper += 1
+    print('''<small><a href="'''+row['URL']+'''">'''+ row['Title'] + "</a></small><i><small><small>"+row['Authors']+'.'+row['Source']+'</small></small></i>\n')
+
