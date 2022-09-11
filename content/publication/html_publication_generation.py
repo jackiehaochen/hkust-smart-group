0c9516092bf3267dd6833ef3a68c913692604861
xiaoyu-z <xiaoyu.z.ch@gmail.com>
2022-09-11 20:11:41 +0800

    update publication page

--- content/publication/html_publication_generation.py
+++ content/publication/html_publication_generation.py
@@ -5,5 +5,5 @@ with open('test.csv', newline='') as csvfile:
   total_paper = 0
   for row in reader:
     total_paper += 1
-    print('''<small><a href="'''+row['URL']+'''">'''+ row['Title'] + "</a></small><i><small><small>"+row['Authors']+'.'+row['Source']+'</small></small></i>\n')
+    print('''<small><a href="'''+row['URL']+'''">'''+ row['Title'] + "</a></small><i><small><small> "+row['Authors']+'.'+row['Source']+'</small></small></i>\n')
 
